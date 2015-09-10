2a7bf28292c8557ab4ecc13ba0962d020f6e62b3
fifiteen82726 <fifiteen82726@gmail.com>
2015-09-10 17:38:30 +0800

    git init

---
+++ Assets/Scripts/ChosePositionAndForce.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChosePositionAndForce : MonoBehaviour {
+
+	public GameObject Star;
+	public iTween.EaseType easeType;
+	public iTween.LoopType loopType;
+	public float startPosition,endPosition;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		iTween.MoveTo (Star, iTween.Hash ("y", endPosition,"speed",speed,"EaseType",easeType,"LoopType",loopType));
+		
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Launcher : MonoBehaviour {
+
+	public iTween.EaseType easeType;
+	public iTween.LoopType loopType;
+	public float startPosition,endPosition;
+	public float speed;
+	 
+	// Use this for initialization
+	void Start () {
+		iTween.MoveTo (gameObject, iTween.Hash ("x", endPosition,"speed",speed,"EaseType",easeType,"LoopType",loopType));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
